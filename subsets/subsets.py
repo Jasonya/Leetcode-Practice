0a7c5f3120daeded1ca4928a9e10dc2893f40d3a
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-06-02 14:43:45 -0400

    Time: 20 ms (72.68%), Space: 13.6 MB (68.78%) - LeetHub

--- subsets/subsets.py
+++ subsets/subsets.py
@@ -5,18 +5,18 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         # dfs/recursion
-#         res = []
-#         self.dfs(nums, 0, res, [])
-#         return res
+        res = []
+        self.dfs(nums, 0, res, [])
+        return res
     
-#     def dfs(self, nums, index, res, cur):
-#         res.append(cur)
-#         for i in range(index, len(nums)):
-#             self.dfs(nums,i+1,res,cur+[nums[i]])
+    def dfs(self, nums, index, res, cur):
+        res.append(cur)
+        for i in range(index, len(nums)):
+            self.dfs(nums,i+1,res,cur+[nums[i]])
                 
         
-        result = [[]]
-        for num in nums:
-            result += [i+[num] for i in result]
-        return result
+        # result = [[]]
+        # for num in nums:
+        #     result += [i+[num] for i in result]
+        # return result
         
