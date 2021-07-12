352cfb0a2792fbc8f50fb8142b7ebdc0791dd2f7
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-07-12 17:05:46 -0400

    Time: 60 ms (75.87%), Space: 18.1 MB (55.89%) - LeetHub

--- min-stack/min-stack.py
+++ min-stack/min-stack.py
@@ -1,38 +1,25 @@
-class MinStack(object):
+class MinStack:
 
     def __init__(self):
         """
         initialize your data structure here.
         """
         self.stack = []
-        self.min_stack = [float('inf')]
+        self.min = [math.inf]
 
-    def push(self, val):
-        """
-        :type val: int
-        :rtype: None
-        """
+    def push(self, val: int) -> None:
         self.stack.append(val)
-        self.min_stack.append(min(self.min_stack[-1],val))
+        self.min.append(min(val, self.min[-1]))
 
-    def pop(self):
-        """
-        :rtype: None
-        """
+    def pop(self) -> None:
         self.stack.pop()
-        self.min_stack.pop()
+        self.min.pop()
 
-    def top(self):
-        """
-        :rtype: int
-        """
+    def top(self) -> int:
         return self.stack[-1]
 
-    def getMin(self):
-        """
-        :rtype: int
-        """
-        return self.min_stack[-1]
+    def getMin(self) -> int:
+        return self.min[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
