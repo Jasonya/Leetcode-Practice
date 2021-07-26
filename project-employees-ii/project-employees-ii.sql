bf9a59a126bf2e242350b2f7849d8ab587185b5a
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-07-26 00:45:14 -0400

    Time: 686 ms (55.61%), Space: 0B (100.00%) - LeetHub

---
+++ project-employees-ii/project-employees-ii.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select project_id
+from (
+select project_id,
+rank() over(order by count(distinct employee_id) desc) as rnk
+from Project
+group by 1) a
+where rnk = 1
+
