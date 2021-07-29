a7ffc93bc4f1079da0cb9f3bacfa813e97458091
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-07-29 19:54:40 -0400

    Time: 377 ms (91.47%), Space: 0B (100.00%) - LeetHub

--- find-customer-referee/find-customer-referee.sql
+++ find-customer-referee/find-customer-referee.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
-SELECT name
-FROM customer
-WHERE referee_id <> 2 or referee_id IS NULL
+# SELECT name
+# FROM customer
+# WHERE referee_id <> 2 or referee_id IS NULL
+
+select name
+from customer
+where referee_id <> 2 or referee_id is null
