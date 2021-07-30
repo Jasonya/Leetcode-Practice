0d252fb0998b5fd8a361110e2cc46c6aa4c46e75
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-07-29 20:31:26 -0400

    Time: 262 ms (52.36%), Space: 0B (100.00%) - LeetHub

--- classes-more-than-5-students/classes-more-than-5-students.sql
+++ classes-more-than-5-students/classes-more-than-5-students.sql
@@ -1,4 +1,12 @@
 # Write your MySQL query statement below
-SELECT class
-FROM courses
-GROUP BY class HAVING count(DISTINCT student) >= 5
+# SELECT class
+# FROM courses
+# # stduent name may dupliacte
+# GROUP BY class HAVING count(DISTINCT student) >= 5
+
+
+
+select class
+from courses
+group by class
+having count(distinct student) >= 5
