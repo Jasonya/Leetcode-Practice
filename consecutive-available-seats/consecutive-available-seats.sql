f4f35eaf4735ac1144e426ad852817b5af0ac3e1
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-07-31 11:32:08 -0400

    Time: 362 ms (31.98%), Space: 0B (100.00%) - LeetHub

--- consecutive-available-seats/consecutive-available-seats.sql
+++ consecutive-available-seats/consecutive-available-seats.sql
@@ -1,6 +1,12 @@
 # Write your MySQL query statement below
-SELECT DISTINCT a.seat_id
-FROM cinema a JOIN cinema b
-WHERE abs(a.seat_id - b.seat_id) = 1 
-AND a.free = 1 and b.free = 1
-ORDER BY 1 ASC
+# SELECT DISTINCT a.seat_id
+# FROM cinema a JOIN cinema b
+# WHERE abs(a.seat_id - b.seat_id) = 1 
+# AND a.free = 1 and b.free = 1
+# ORDER BY 1 ASC
+
+select distinct c1.seat_id
+from cinema c1 join cinema c2
+where abs(c1.seat_id - c2.seat_id) = 1
+and c1.free = 1 and c2.free = 1
+order by seat_id
