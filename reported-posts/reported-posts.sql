79bb24b053e62100e7a43a856011ada0e90a717a
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-08-06 01:49:47 +0800

    Time: 552 ms (40.41%), Space: 0B (100.00%) - LeetHub

--- reported-posts/reported-posts.sql
+++ reported-posts/reported-posts.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
-select extra as "report_reason",
-    count(DISTINCT post_id) as "report_count"
+select extra as report_reason, count(distinct post_id) as  report_count
 from Actions
-where action_date = '2019-07-04' and action = 'report' 
-group by extra
+where datediff('2019-07-05', action_date) = 1 and action = 'report'
+group by 1
+
+
