4a952111822123dacdafcf61c8f3426e62820dc1
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-08-10 12:44:27 +0800

    Time: 919 ms (17.38%), Space: 0B (100.00%) - LeetHub

--- ad-free-sessions/ad-free-sessions.sql
+++ ad-free-sessions/ad-free-sessions.sql
@@ -5,4 +5,8 @@ where session_id not in(
     select p.session_id
     from Playback p left join Ads a
     on p.customer_id = a.customer_id
-    where a.timestamp >= p.start_time and a.timestamp <= p.end_time) 
+    where a.timestamp between p.start_time and p.end_time)
+
+
+
+
