6edc93f443df2baea046f9eb448ca28c811578c9
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-10-01 12:00:16 +0800

    Time: 197 ms (12.91%), Space: 20 MB (80.23%) - LeetHub

--- contains-duplicate/contains-duplicate.py
+++ contains-duplicate/contains-duplicate.py
@@ -1,23 +1,11 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        # set
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
         return len(set(nums)) != len(nums)
+    
+                
         
-        # set2
-        # s = set()
-        # for num in nums:
-        #     if num in s:
-        #         return True
-        #     s.add(num)
-        # return False
-        
-        # sort space-time trade off
-        # nums.sort()
-        # for i in range(len(nums)-1):
-        #     if nums[i] == nums[i+1]:
-        #         return True
-        # return False
+        # hash table
+        # dict = {}
+        # for i in range(len(nums)):
+        #     dict
+            
