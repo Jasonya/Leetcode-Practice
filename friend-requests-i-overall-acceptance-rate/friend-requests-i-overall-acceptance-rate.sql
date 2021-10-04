f8b3b191effe781fdbb1f0c69f2b7f27b3b72ad2
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-10-04 11:57:17 +0800

    Time: 511 ms (32.76%), Space: 0B (100.00%) - LeetHub

--- friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
+++ friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
@@ -1,8 +1,7 @@
 # Write your MySQL query statement below
 # Distinct xx, xx return the uniqle composition
-SELECT 
-ROUND (
-        IFNULL(
-            (SELECT count(DISTINCT requester_id, accepter_id) FROM RequestAccepted) /
-            (SELECT count(DISTINCT sender_id, send_to_id) FROM FriendRequest), 0)
-        , 2) AS accept_rate
+select
+ifnull(round((select count(distinct requester_id, accepter_id) from RequestAccepted) 
+/
+(select count(distinct sender_id, send_to_id) from FriendRequest), 2),0) as accept_rate
+        
