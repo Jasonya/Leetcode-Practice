5ada236d1f24921a306a376de485edab7f39f7e4
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-10-09 04:07:02 +0800

    Time: 927 ms (87.66%), Space: 0B (100.00%) - LeetHub

--- sales-analysis-i/sales-analysis-i.sql
+++ sales-analysis-i/sales-analysis-i.sql
@@ -1,10 +1,11 @@
 # Write your MySQL query statement below
 select seller_id from(
-select seller_id,
-    rank() over(order by sum(price) desc) as rnk 
+    select seller_id,
+     rank() over(order by sum(price) desc) as rnk
     from Sales
-    group by 1) a
-where rnk = 1
+    group by 1) sub
+where rnk=1
+
 
 
 
