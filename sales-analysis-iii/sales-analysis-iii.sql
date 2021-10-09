fc1fd1b77ca1b5c7a39db4402b3d0c8f792cdf2e
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-10-10 00:10:07 +0800

    Time: 996 ms (67.20%), Space: 0B (100.00%) - LeetHub

--- sales-analysis-iii/sales-analysis-iii.sql
+++ sales-analysis-iii/sales-analysis-iii.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
-SELECT DISTINCT(s.product_id), p.product_name
-FROM Sales s LEFT JOIN Product P
-ON s.product_id = p.product_id
-WHERE s.product_id not in (SELECT
-        product_id from Sales 
-        where sale_date < '2019-01-01' OR sale_date > '2019-03-31')
+select s.product_id, p.product_name
+from Sales s left join Product p
+on s.product_id = p.product_id
+group by product_id
+having max(sale_date) <= '2019-03-31' and min(sale_date) >= '2019-01-01'
