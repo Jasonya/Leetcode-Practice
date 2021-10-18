3325c0c62c84d87cb87131f92c31214e97a0ff2a
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-10-18 10:19:16 +0800

    Time: 44 ms (79.22%), Space: 16.4 MB (80.85%) - LeetHub

--- validate-binary-search-tree/validate-binary-search-tree.py
+++ validate-binary-search-tree/validate-binary-search-tree.py
@@ -6,14 +6,12 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def helper(n, low, high):
-            if not n:
-                return True
-            elif low is not None and n.val <= low:
-                return False
-            elif high is not None and n.val >= high:
-                return False
-            return helper(n.left, low, n.val) and helper(n.right, n.val, high)
-        
-        return helper(root, None, None)
-        
+            def helper(n, low, high):
+                if not n:
+                    return True
+                elif low is not None and n.val <= low:
+                    return False
+                elif high is not None and n.val >= high:
+                    return False
+                return helper(n.left, low, n.val) and helper(n.right, n.val, high)
+            return helper(root, None, None)
