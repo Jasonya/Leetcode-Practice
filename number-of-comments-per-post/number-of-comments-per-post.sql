a351344f00e7c502f7e03c843f7907799d62ad1a
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-10-18 12:07:28 +0800

    Time: 1103 ms (77.28%), Space: 0B (100.00%) - LeetHub

--- number-of-comments-per-post/number-of-comments-per-post.sql
+++ number-of-comments-per-post/number-of-comments-per-post.sql
@@ -1,16 +1,18 @@
 # Write your MySQL query statement below
-with post as (
-select sub_id as post_id
-from Submissions
-where parent_id is null
-group by 1),
+with post as(
+    select sub_id
+    from Submissions
+    where parent_id is null
+    group by 1
+), comments as(
+    select parent_id as sub_id,
+        count(distinct sub_id) as cnt
+    from Submissions
+    group by 1
+)
 
-comment as (
-select count(distinct sub_id) as cnt, parent_id as post_id
-from Submissions 
-group by 2)
-
-select p.post_id, ifnull(c.cnt, 0) as number_of_comments
-from post p left join comment c
-on p.post_id = c.post_id
+select p.sub_id as post_id, ifnull(cnt, 0) as number_of_comments
+from post p left join comments c
+on p.sub_id = c.sub_id
 order by 1
+
