4ff621915e5b731d47e3210b8b69902e0a64666b
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-10-21 06:57:18 +0800

    Time: 470 ms (80.08%), Space: 0B (100.00%) - LeetHub

--- weather-type-in-each-country/weather-type-in-each-country.sql
+++ weather-type-in-each-country/weather-type-in-each-country.sql
@@ -1,9 +1,10 @@
 # Write your MySQL query statement below
-select country_name, 
-    case when avg(w.weather_state) <= 15 then 'Cold'
-        when avg(w.weather_state) >= 25 then 'Hot'
-        else 'Warm' end as weather_type
-from Weather w left join Countries c
-on w.country_id = c.country_id
-where date_format(w.day, '%Y-%m') = '2019-11'
+select c.country_name,
+    case 
+        when avg(weather_state) <= 15 then 'Cold' 
+        when avg(weather_state) >= 25 then 'Hot'
+        else 'Warm' end as weather_type 
+from Countries c left join Weather w
+on c.country_id = w.country_id
+where left(day, 7) = '2019-11'
 group by 1
