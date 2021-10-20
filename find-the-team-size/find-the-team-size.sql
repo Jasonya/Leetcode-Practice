4c22b0edaff68404ee647f4e13b706422c98498d
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-10-21 06:59:38 +0800

    Time: 269 ms (51.62%), Space: 0B (100.00%) - LeetHub

--- find-the-team-size/find-the-team-size.sql
+++ find-the-team-size/find-the-team-size.sql
@@ -1,8 +1,7 @@
 # Write your MySQL query statement below
-select e.employee_id, sub.ts as team_size
-from Employee e left join (
-select team_id,
-    count(employee_id) as ts
-from Employee 
-group by team_id) sub
+select e.employee_id, cnt as team_size
+from Employee e left join(
+    select team_id, count(team_id) as cnt
+    from Employee
+    group by 1) sub
 on e.team_id = sub.team_id
