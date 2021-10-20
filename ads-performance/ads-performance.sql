86e17fbf05466c98b5108abd207e6166d5750686
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-10-21 07:10:08 +0800

    Time: 731 ms (8.56%), Space: 0B (100.00%) - LeetHub

--- ads-performance/ads-performance.sql
+++ ads-performance/ads-performance.sql
@@ -1,12 +1,11 @@
 # Write your MySQL query statement below
-with cte as(
 select ad_id,
-count(case action when 'Clicked' then 1 else null end) as Cliecked,
-count(case action when 'Ingored' then null else null end) as Ignored,
-count(case action when 'Viewed' then 1 else null end) as Viewed
-from Ads group by 1)
-
-select ad_id,
-    ifnull(round(Cliecked/(Cliecked+Viewed)*100, 2),0) as ctr
-from cte
+    round(ifnull(sum(case when action='Clicked' then 1 else 0 end)/
+    sum(case when action='Clicked' then 1 
+             when action='Viewed' then 1
+             else 0 end),0)*100,2) as ctr
+from Ads
+group by 1
 order by 2 desc, 1 asc
+
+
