8989af5ca36eec0956d03a8303d11a4356b4648b
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-10-24 06:18:04 +0800

    Time: 793 ms (41.21%), Space: 0B (100.00%) - LeetHub

--- npv-queries/npv-queries.sql
+++ npv-queries/npv-queries.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-select q.id, q.year,
-    ifnull(n.npv, 0) as npv
+select q.id, q.year, ifnull(n.npv,0) as npv
 from Queries q left join NPV n
 on q.id = n.id and q.year = n.year
