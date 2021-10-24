2d12600f76d904e7b6f233d90c86e769d56ee933
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-10-25 02:49:10 +0800

    Time: 1779 ms (5.07%), Space: 0B (100.00%) - LeetHub

--- customer-order-frequency/customer-order-frequency.sql
+++ customer-order-frequency/customer-order-frequency.sql
@@ -1,13 +1,18 @@
 # Write your MySQL query statement below
-select sub.customer_id, c.name from(
-select s.customer_id, left(s.order_date, 7) as "data",
-    sum(s.quantity * p.price)
-from Orders s left join Product p
-on s.product_id = p.product_id
-where date_format(s.order_date, '%Y-%m') between '2020-06' and '2020-07' 
-group by 1, 2
-having sum(s.quantity * p.price)>=100) sub
-left join Customers c
-on sub.customer_id = c.customer_id
-group by sub.customer_id
-having count(sub.customer_id) >= 2
+with cte as(
+    select date_format(order_date, '%Y-%m') as t, customer_id,
+        sum(quantity*price) as spend
+    from Orders o left join Product p
+    on o.product_id = p.product_id
+    group by 1,2
+)
+
+select s.customer_id, name
+from Customers s join(
+    select *
+    from cte 
+    where t between '2020-06' and '2020-07' and spend >= 100
+    group by customer_id having count(*) = 2) sub
+on sub.customer_id = s.customer_id
+
+
