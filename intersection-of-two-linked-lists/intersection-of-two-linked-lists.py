3691674432fc05332ab3f9a5aa375ab6da4d2075
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-10-29 23:04:38 +0800

    Time: 156 ms (91.54%), Space: 29.4 MB (54.34%) - LeetHub

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -8,11 +8,12 @@ class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
         # two pointers go thourght the linked list once, will get
         # the length differnct between A and B
-        pA = headA
-        pB = headB
+        p1 = headA
+        p2 = headB
         
-        while pA != pB:
-            pA = pA.next if pA else headB
-            pB = pB.next if pB else headA
-        return pA
+        while p1 != p2:
+            p1 = p1.next if p1 else headB
+            p2 = p2.next if p2 else headA
+        return p1
         
+   
