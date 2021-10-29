bc5f36d4aeca0fdb6d1987a3ee25ec689b0108bf
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-10-30 00:29:42 +0800

    Time: 611 ms (76.44%), Space: 0B (100.00%) - LeetHub

--- products-worth-over-invoices/products-worth-over-invoices.sql
+++ products-worth-over-invoices/products-worth-over-invoices.sql
@@ -1,10 +1,12 @@
 # Write your MySQL query statement below
-select p.name,
+select name,
     ifnull(sum(rest), 0) as rest,
-    ifnull(sum(paid), 0) as paid, 
-    ifnull(sum(canceled), 0) as canceled,
-    ifnull(sum(refunded), 0) as refunded
+    ifnull(sum(paid),0) as paid,
+    ifnull(sum(canceled),0) as canceled,
+    ifnull(sum(refunded),0) as refunded
 from Product p left join Invoice i
-on p.product_id = i.product_id 
-group by p.product_id
-order by p.name
+on i.product_id = p.product_id
+group by 1
+order by 1
+
+
