d59be7467a2c7d2791c299542b8c270e93b644c5
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-10-31 13:24:05 +0800

    Time: 501 ms (89.37%), Space: 0B (100.00%) - LeetHub

--- the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -5,7 +5,7 @@ select e1.employee_id,
     round(avg(e2.age), 0) as average_age
 from Employees e1 join Employees e2
 on e1.employee_id = e2.reports_to
-group by e1.employee_id
+group by 1
 order by 1
 
 
