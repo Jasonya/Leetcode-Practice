51ae01ecb3e11cf45ff6625223e15b3cc55a4443
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-11-01 02:32:24 +0800

    Time: 429 ms (84.62%), Space: 0B (100.00%) - LeetHub

--- products-price-for-each-store/products-price-for-each-store.sql
+++ products-price-for-each-store/products-price-for-each-store.sql
@@ -1,9 +1,16 @@
-/* Write your T-SQL query statement below */
-select *
-from(
-    select product_id, store, price from Products
-)T1
-PIVOT
-(max(price) for store in ( [store1], [store2], [store3])
+# Write your MySQL query statement below
+# select product_id,
+#     max(if(store = 'store1', price, null)) as "store1",
+#     max(if(store = 'store2', price, null)) as "store2",
+#     max(if(store = 'store3', price, null))as "store3"
+# from Products
+# group by product_id
 
-)T2
+
+
+select product_id,
+    max(case when store='store1' then price end) as 'store1',
+    max(case when store='store2' then price end) as 'store2',
+    max(case when store='store3' then price end) as 'store3'
+from Products
+group by 1
