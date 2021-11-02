a60472e27fb34c4aff300d115e2e4ef35b94e170
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-11-02 22:49:35 +0800

    Time: 393 ms (98.97%), Space: 0B (100.00%) - LeetHub

--- rearrange-products-table/rearrange-products-table.sql
+++ rearrange-products-table/rearrange-products-table.sql
@@ -1,12 +1,21 @@
 # Write your MySQL query statement below
-select* from(
-    select Products.product_id,
-        stores.store,
-        case 
-            when store='store1' then store1
-            when store='store2' then store2
-            when store='store3' then store3 end as price from(
-                select 'store1' as store union
-                select 'store2' as store union
-                select 'store3' as store) as stores cross join Products) as Result
-                    where price is not null
+# select* from(
+#     select Products.product_id,
+#         stores.store,
+#         case 
+#             when store='store1' then store1
+#             when store='store2' then store2
+#             when store='store3' then store3 end as price from(
+#                 select 'store1' as store union
+#                 select 'store2' as store union
+#                 select 'store3' as store) as stores cross join Products) as Result
+#                     where price is not null
+
+with Summery as (
+select product_id, 'store1' as store, store1 as price from Products
+union all
+select product_id, 'store2' as store, store2 as price from Products
+union all
+select product_id, 'store3' as sotre, store3 as price from Products)
+
+select product_id, store, price from Summery where price is not null
