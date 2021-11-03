fd46cbd421612ed62fba8a851533fcd56dc32e21
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-11-03 12:55:00 +0800

    Time: 701 ms (79.48%), Space: 0B (100.00%) - LeetHub

--- ad-free-sessions/ad-free-sessions.sql
+++ ad-free-sessions/ad-free-sessions.sql
@@ -1,12 +1,9 @@
 # Write your MySQL query statement below
-select session_id 
+select session_id
 from Playback
 where session_id not in(
     select p.session_id
     from Playback p left join Ads a
     on p.customer_id = a.customer_id
-    where a.timestamp between p.start_time and p.end_time)
-
-
-
+    where timestamp between start_time and end_time)
 
