88b51a10dd50585fba108d3b3b7fa423128ef7f3
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-11-04 11:00:30 +0800

    Time: 467 ms (62.25%), Space: 0B (100.00%) - LeetHub

--- convert-date-format/convert-date-format.sql
+++ convert-date-format/convert-date-format.sql
@@ -1,6 +1,10 @@
 # Write your MySQL query statement below
 
 
+
+
+
+
 select concat(dayname(day), ', ', monthname(day), ' ', dayofmonth(day), ', ',
-              year(day)) as day
+            year(day)) as day
 from Days
