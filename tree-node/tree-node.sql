e3c2fd14b647ccc2d126b6f47a73a536fed83011
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-11-06 02:35:48 +0800

    Time: 386 ms (74.27%), Space: 0B (100.00%) - LeetHub

--- tree-node/tree-node.sql
+++ tree-node/tree-node.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 select id,
     case when p_id is null then 'Root'
-         when p_id is not null and id not in (select distinct p_id from tree
-                                             where p_id is not null) then 'Leaf'
-         else 'Inner' end as Type
+         when p_id is not null and id not in (select p_id from tree where p_id is not null) then 'Leaf'
+    else 'Inner' end as type
 from tree
-order by 1
+order by 1 asc
+
