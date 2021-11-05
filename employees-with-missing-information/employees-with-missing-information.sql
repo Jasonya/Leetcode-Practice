e1ffb74f77032680c55c847bff9a71d3840ae8b2
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-11-06 02:17:35 +0800

    Time: 421 ms (92.46%), Space: 0B (100.00%) - LeetHub

--- employees-with-missing-information/employees-with-missing-information.sql
+++ employees-with-missing-information/employees-with-missing-information.sql
@@ -1,11 +1,12 @@
 # Write your MySQL query statement below
-(select e.employee_id
+select e.employee_id
 from Employees e left join Salaries s
 on e.employee_id = s.employee_id
-where s.employee_id is null)
-union (
-select s.employee_Id
-    from Salaries s left join Employees e
-    on s.employee_id = e.employee_id
-    where e.employee_id is null)
-order by 1
+where s.employee_id is null
+union 
+select s.employee_id
+from Salaries s left join Employees e
+on s.employee_id = e.employee_id
+where e.employee_id is null
+order by 1 asc
+
