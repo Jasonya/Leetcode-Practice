e3e25f114e58ee93aa8bca477409b91d8de22edf
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-11-07 09:53:42 +0800

    Time: 195 ms (86.33%), Space: 0B (100.00%) - LeetHub

--- shortest-distance-in-a-plane/shortest-distance-in-a-plane.sql
+++ shortest-distance-in-a-plane/shortest-distance-in-a-plane.sql
@@ -1,4 +1,7 @@
 # Write your MySQL query statement below
-select min(round(sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2)), 2)) as shortest
-from point_2d p1, point_2d p2
-where (p1.x, p1.y) != (p2.x, p2.y)
+select min(round(sqrt(pow(p1.x-p2.x, 2) + pow(p1.y-p2.y,2)),2)) as shortest
+from Point2D p1 join Point2D p2
+where (p1.x, p1.y) <> (p2.x, p2.y)
+
+
+
