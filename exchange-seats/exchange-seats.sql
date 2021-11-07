87b23ca9c57409c25281a0646ed5619c7338c8d8
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-11-08 07:39:09 +0800

    Time: 484 ms (8.13%), Space: 0B (100.00%) - LeetHub

--- exchange-seats/exchange-seats.sql
+++ exchange-seats/exchange-seats.sql
@@ -1,12 +1,10 @@
 # Write your MySQL query statement below
-select 
-    id,
-    # shift student column down then up
-    if(id%2=0, 
-       lag(student, 1, student) over (order by id asc),  
-       lag(student, 1, student) over (order by id desc)) as student
-from seat
-order by id asc
+select id,
+if (id%2 = 0,
+    lag(student, 1, student) over(order by id asc),
+    lag(student, 1, student) over(order by id desc)) as student
+from Seat
+order by 1
 
 
 
