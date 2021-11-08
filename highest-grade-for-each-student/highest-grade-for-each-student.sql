9c711753e3d906e4aa1aea604569b25660cc2a8f
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-11-09 01:03:55 +0800

    Time: 1060 ms (5.02%), Space: 0B (100.00%) - LeetHub

--- highest-grade-for-each-student/highest-grade-for-each-student.sql
+++ highest-grade-for-each-student/highest-grade-for-each-student.sql
@@ -2,8 +2,10 @@
 select student_id, min(course_id) as course_id, grade
 from Enrollments
 where (student_id, grade) in (
-select student_id, max(grade)
-from Enrollments
-group by student_id)
-group by 1
-order by 1
+    select student_id, max(grade) as grade
+    from Enrollments 
+    group by 1)
+group by 1 
+order by 1 asc
+
+
