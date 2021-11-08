efda806df87983011d0b95c486e540c361582635
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-11-09 01:43:23 +0800

    Time: 1134 ms (20.44%), Space: 0B (100.00%) - LeetHub

--- reported-posts-ii/reported-posts-ii.sql
+++ reported-posts-ii/reported-posts-ii.sql
@@ -1,9 +1,8 @@
 # Write your MySQL query statement below
-
-select
-    round(avg(cnt),2) as average_daily_percent from(
-select count(distinct r.post_id)/ count(distinct a.post_id)*100 as cnt
-from Actions a left join Removals r
-on a.post_id = r.post_id
-where action = 'report' and extra = 'spam'
-group by a.action_date) sub
+select round(avg(percentage), 2) as average_daily_percent
+from(
+    select count(distinct r.post_id)/count(distinct a.post_id)*100 as percentage
+    from Actions a left join Removals r
+    on a.post_id = r.post_id
+    where action='report' and extra='spam'
+    group by action_date) sub
