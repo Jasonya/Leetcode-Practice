52d6fcc911724e138f145d9eac5e5bedf76c597b
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-11-10 13:33:54 +0800

    Time: 1074 ms (54.20%), Space: 0B (100.00%) - LeetHub

--- market-analysis-i/market-analysis-i.sql
+++ market-analysis-i/market-analysis-i.sql
@@ -8,8 +8,10 @@
 
 
 
-select user_id as buyer_id, join_date, 
+select u.user_id as buyer_id, join_date,
     ifnull(count(order_date), 0) as orders_in_2019
 from Users u left join Orders o
 on u.user_id = o.buyer_id and year(order_date) = '2019'
 group by 1
+
+
