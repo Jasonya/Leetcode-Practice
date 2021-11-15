9c9ef7a034a5aaabfe59bb03d671e738c3d47055
Jasonya <76511727+Jasonya@users.noreply.github.com>
2021-11-15 10:38:37 +0800

    Time: 859 ms (11.68%), Space: 0B (100.00%) - LeetHub

--- monthly-transactions-ii/monthly-transactions-ii.sql
+++ monthly-transactions-ii/monthly-transactions-ii.sql
@@ -1,17 +1,24 @@
 # Write your MySQL query statement below
-select 
-    date_format(trans_date, '%Y-%m') as month,
-    country,
-    sum(state='approved') as approved_count,
-    sum(if(state='approved',amount, 0)) as approved_amount,
-    sum(state='chargeback') as chargeback_count,
-    sum(if(state='chargeback', amount, 0)) as chargeback_amount
-    
-from 
-    (select distinct * from Transactions where state='approved'
-    union all
-    select distinct c.trans_id as id, t.country, 'chargeback' as state, t.amount, c.trans_date 
-     from Chargebacks c left join Transactions t
-     on c.trans_id = t.id) temp
-group by 1, 2
+with cte as (
+    select distinct * from Transactions where state='approved'
+    union 
+    select distinct c.trans_id as id, country, 'chargebacks' as state, amount, c.trans_date
+    from Chargebacks c left join Transactions t
+    on t.id = c.trans_id
+)
+
+
+
+select date_format(trans_date, '%Y-%m') as month, country,
+    sum(case when state='approved' then 1 else 0 end) as approved_count,
+    sum(case when state='approved' then amount else 0 end) as approved_amount,
+    sum(case when state='chargebacks' then 1 else 0 end) as chargeback_count,
+    sum(case when state='chargebacks' then amount else 0 end) as chargeback_amount
+from cte
+group by 1,2
+
+
+
+
+
 
